e5ffc07900b5833d84b933451ef64fd44c5f3999
Sophistt <408876735@qq.com>
2020-05-17 14:49:30 +0800

    remove windows executable and bugs fixed

--- C++/C-plus-learn/wengkai/b.cpp
+++ C++/C-plus-learn/wengkai/b.cpp
@@ -8,8 +8,8 @@ class B
 private:
     string name;
 public:
-    // 在析构函数后面采用 :成员变量名(值) 的方式来初始化变量，而不是在析构函数里赋值
-    B(string s):name(s) 
+    // 在构造函数后面采用 :成员变量名(值) 的方式来初始化变量，而不是在构造函数里赋值
+    B(string s):name(s)
     {
         cout << name << endl;
         cout << "construct funciton..." << endl;
