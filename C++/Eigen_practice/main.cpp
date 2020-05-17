98304443b008db6bca433a7cfb27b005872680ce
Sophistt <408876735@qq.com>
2020-05-17 14:48:57 +0800

    Eigen Init

---
+++ C++/Eigen_practice/main.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <iostream>
+#include <Eigen\Dense>
+
+using Eigen::MatrixXd;
+
+int main()
+{
+    MatrixXd m = MatrixXd::Random(3, 3);
+    m = (m + MatrixXd::Constant(3, 3, 1.2)) * 50;
+    std::cout << "m = " << std::endl << m << std::endl;
+
+    Eigen::VectorXd v(3);
+    v << 1, 2, 3;
+    std::cout << "m * v = " << std::endl << m * v << std::endl;
+    
+    return 0;
+}
+
